897ef9ea76c72af89da6bc395d99f1864bbd11ad
A00276551 <a00276551@student.ait.ie>
2021-09-24 12:07:04 +0100

    Flow

---
+++ src/FlowLayoutExample.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class FlowLayoutExample extends JFrame {
+    JButton b1 = new JButton("Button 1");
+    JButton b2 = new JButton("2");
+    JButton b3 = new JButton("Button 3");
+    JButton b4 = new JButton("Long named Button 4");
+    JButton b5 = new JButton("Button 5");
+
+    public FlowLayoutExample() {
+        getContentPane().setLayout(new FlowLayout());
+
+        getContentPane().add(b1);
+        getContentPane().add(b2);
+        getContentPane().add(b3);
+        getContentPane().add(b4);
+        getContentPane().add(b5);
+        setSize(500, 75);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new FlowLayoutExample();
+    }
+
+}
